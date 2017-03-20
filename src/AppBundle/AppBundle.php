a2e9d537d7f3c5a45ebc857c6dbe573ec2f4bdba
Stepan Yudin <stepan.sib@gmail.com>
2017-03-20 23:38:13 +0300

    disabled compiler pass

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -8,10 +8,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
 
-        //$container->addCompilerPass(new SetSwiftmailerInsecurePass());
-    }
 }
