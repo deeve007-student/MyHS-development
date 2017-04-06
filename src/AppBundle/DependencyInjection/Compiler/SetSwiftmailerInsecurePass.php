483a48a7bb534c221efe61d919983f311b86a4ce
Stepan Yudin <stepan.sib@gmail.com>
2017-04-06 16:21:10 +0300

    dev env subdomains handling

--- src/AppBundle/DependencyInjection/Compiler/SetSwiftmailerInsecurePass.php
+++ src/AppBundle/DependencyInjection/Compiler/SetSwiftmailerInsecurePass.php
@@ -6,11 +6,6 @@ use AppBundle\Mailer\InsecureTransport;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-/**
- * RegisterPluginsPass registers Swiftmailer plugins.
- *
- * @author Fabien Potencier <fabien@symfony.com>
- */
 class SetSwiftmailerInsecurePass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
