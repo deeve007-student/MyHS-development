6383e05c667038d95f095b2ee470ad8937bb2186
Stepan Yudin <stepan.sib@gmail.com>
2017-04-20 14:40:28 +0300

    added patient mobile menu

---
+++ src/AppBundle/Menu/PatientMobileMenuBuilder.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stepan Yudin <stepan.sib@gmail.com>
+ * Date: 20.04.2017
+ * Time: 14:35
+ */
+
+namespace AppBundle\Menu;
+
+use AppBundle\Entity\Patient;
+use AppBundle\Utils\Hasher;
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class PatientMobileMenuBuilder extends PatientMenuBuilder
+{
+    public function createMenu(array $options)
+    {
+        $menu = parent::createMenu($options);
+        $menu->setChildrenAttribute('class', 'dropdown-menu');
+        return $menu;
+    }
+}
+
