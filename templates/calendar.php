aa87fe1605ea74b690cdccbcc7d2e42961104372
Dave R <dave@zavadesign.com>
2017-06-20 10:00:35 -0300

    Icon updates

--- templates/calendar.php
+++ templates/calendar.php
@@ -1,7 +1,17 @@
 <?php include 'includes/header.php'; ?>
 				
-			<h1 class="page-title title-dashboard">Calendar</h1>
+			<h1 class="page-title title-calendar">Calendar</h1>
 			
+			<div class="page-actions">
+				<!-- graphic is just for demo purposes -->
+				<img src="img/temp-calendar-butts.png">
+			</div>
+			
+			<section class="content-panel">
+				[ Calendar here - No date range title above calendar ]
+			</section>
+			
+			<h5>Demo links</h5>
 			<a href="#" class="delete-item" data-toggle="modal" data-target=".modal-appointment">Create new appointment</a><br>
 			<a href="#" class="delete-item" data-toggle="modal" data-target=".modal-appointment-view">View current appointment</a><br>
 			<a href="#" class="delete-item" data-toggle="modal" data-target=".modal-unavailable">Create unavailable block</a>
