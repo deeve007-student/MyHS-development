6e183381a5451f63b7405d1cf76ffe1d3be8af3c
Dave R <dave@zavadesign.com>
2017-07-12 18:36:56 -0300

    Treatment notes changes

--- templates/index.php
+++ templates/index.php
@@ -162,6 +162,77 @@
 				
 				<div class="col-lg-6 dash-col2">
 				
+					<?php // start communication list ?>
+					<section class="content-panel dashboard-panel communications-list">
+						<h2 class="panel-title">Latest communications</h2>
+						
+						<table class="table communications-list">
+					
+							<tr class="table-row table-header">
+								<td class="table-cell"><!-- channel --></td>
+								<td class="table-cell">Date</td>
+								<td class="table-cell">Name</td>
+								<td class="table-cell">Category</td>
+							</tr>
+							
+							<tr class="table-row">
+								<td class="table-cell"><i class="fa fa-envelope-o" aria-hidden="true"></i></td>
+								<td class="table-cell">24 Aug 2017</td>
+								<td class="table-cell">Bill Smith</td>
+								<td class="table-cell">Invoice</td>
+							</tr>
+							
+							<tr class="table-row">
+								<td class="table-cell"><i class="fa fa-mobile" aria-hidden="true"></i></td>
+								<td class="table-cell">12 Aug 2017</td>
+								<td class="table-cell">John Jones</td>
+								<td class="table-cell">Appointment reminder</td>
+							</tr>
+							
+							<tr class="table-row sms-reply">
+								<td class="table-cell"><i class="fa fa-share" aria-hidden="true"></i></td>
+								<td class="table-cell">12 Aug 2017</td>
+								<td class="table-cell sms-message" colspan="2">Sorry I can't make this, can I rebook for August 20? Thanks</td>
+							</tr>
+							
+							<tr class="table-row">
+								<td class="table-cell"><i class="fa fa-phone" aria-hidden="true"></i></td>
+								<td class="table-cell">24 Jul 2017</td>
+								<td class="table-cell">Billy Joel</td>
+								<td class="table-cell">Recall</td>
+							</tr>
+							
+							<tr class="table-row">
+								<td class="table-cell"><i class="fa fa-envelope-o" aria-hidden="true"></i></td>
+								<td class="table-cell">24 Jun 2017</td>
+								<td class="table-cell">Bill Smith</td>
+								<td class="table-cell">Invoice</td>
+							</tr>
+							
+							<tr class="table-row">
+								<td class="table-cell"><i class="fa fa-mobile" aria-hidden="true"></i></td>
+								<td class="table-cell">12 Jun 2017</td>
+								<td class="table-cell">John Jones</td>
+								<td class="table-cell">Appointment creation</td>
+							</tr>
+							
+							<tr class="table-row">
+								<td class="table-cell"><i class="fa fa-phone" aria-hidden="true"></i></td>
+								<td class="table-cell">24 May 2017</td>
+								<td class="table-cell">Billy Joel</td>
+								<td class="table-cell">Recall</td>
+							</tr>
+							
+						</table>
+						
+						<a href="communications.php" class="view-all">View all communications</a>
+						
+					</section>
+					<?php // end communication list ?>
+					
+					
+					
+					
 					<?php // start task list ?>
 					<section class="content-panel dashboard-panel task-list">
 						<h2 class="panel-title">Task list</h2>
