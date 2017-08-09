d933de14534fa7fe3b61ae0b839f2ce3ffa25506
Stepan Yudin <stepan.sib@gmail.com>
2017-08-09 22:31:12 +0300

    QB fixed

--- src/AppBundle/Utils/TaskUtils.php
+++ src/AppBundle/Utils/TaskUtils.php
@@ -48,8 +48,8 @@ class TaskUtils
             $startDate = $rTask->getStartDate();
             if (
             $existedTasks = $this->em->getRepository('AppBundle:Task')->createQueryBuilder('t')
-                ->where('t.recurringTask = rTask')
-                ->setParameter('rTask',$rTask)
+                ->where('t.recurringTask = :rTask')
+                ->setParameter('rTask', $rTask)
                 ->orderBy('t.date', 'DESC')->getQuery()->getResult()
             ) {
                 $startDate = $existedTasks[0]->getDate();
