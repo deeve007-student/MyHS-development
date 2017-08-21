93ca1fc1495a3f9717843e8568587090123e0d96
Stepan Yudin <stepan.sib@gmail.com>
2017-08-21 19:15:53 +0300

    Bugfixes etc

--- src/AppBundle/Utils/EventUtils.php
+++ src/AppBundle/Utils/EventUtils.php
@@ -137,7 +137,51 @@ class EventUtils
         return $dt->format('H:i');
     }
 
-    public function calculateFontColor($backgroundColor)
+    public function calculateFontColor($hexColor)
+    {
+        $hexColor = preg_replace('/#+/','',$hexColor);
+
+        //////////// hexColor RGB
+        $R1 = hexdec(substr($hexColor, 0, 2));
+        $G1 = hexdec(substr($hexColor, 2, 2));
+        $B1 = hexdec(substr($hexColor, 4, 2));
+
+        //////////// Black RGB
+        $blackColor = "#000000";
+        $R2BlackColor = hexdec(substr($blackColor, 0, 2));
+        $G2BlackColor = hexdec(substr($blackColor, 2, 2));
+        $B2BlackColor = hexdec(substr($blackColor, 4, 2));
+
+        //////////// Calc contrast ratio
+        $L1 = 0.2126 * pow($R1 / 255, 2.2) +
+            0.7152 * pow($G1 / 255, 2.2) +
+            0.0722 * pow($B1 / 255, 2.2);
+
+        $L2 = 0.2126 * pow($R2BlackColor / 255, 2.2) +
+            0.7152 * pow($G2BlackColor / 255, 2.2) +
+            0.0722 * pow($B2BlackColor / 255, 2.2);
+
+        $contrastRatio = 0;
+        if ($L1 > $L2) {
+            $contrastRatio = (int)(($L1 + 0.05) / ($L2 + 0.05));
+        } else {
+            $contrastRatio = (int)(($L2 + 0.05) / ($L1 + 0.05));
+        }
+
+        //////////// If contrast is more than 5, return black color
+        if ($contrastRatio > 5) {
+            return '#000000';
+        } else { //////////// if not, return white color.
+            return '#ffffff';
+        }
+    }
+
+    /**
+     * @param $backgroundColor
+     * @return string
+     * @deprecated Old version, remove
+     */
+    public function calculateFontColorOld($backgroundColor)
     {
         $r = mb_substr($backgroundColor, 1, 2);
         $g = mb_substr($backgroundColor, 3, 2);
