fdc0c59be17853bf5b132d60ad2765919502dc5c
Dave R <dave@zavadesign.com>
2017-11-19 22:22:57 -0300

    Added reports templates

---
+++ templates/includes/reports-menu-mobile.php
@@ -0,0 +1,8 @@
+						<ul class="dropdown-menu">
+							<li><a href="#">Appointments</a></li>
+								<li><a href="#">Patients</a></li>
+								<li><a href="#">Invoices</a></li>
+								<li><a href="#">Revenue summary</a></li>
+								<li><a href="#">Products</a></li>
+								<li><a href="#">Products (who purchased)</a></li>
+						</ul>
